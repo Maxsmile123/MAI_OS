1389575ec16d7b4ffcad95b5b55f431c93a95292
Maxsmile123 <sysoevmaksim27@gmail.com>
2021-10-11 19:49:50 +0300

    Release version

--- lab2/lab2.c
+++ lab2/lab2.c
@@ -18,16 +18,16 @@ void Reverse(char name[])
 }
 
 char* get_string(int *len) {
-
     *len = 0;
     int capacity = 1; // 1, так как точно будет '\0'
     char *s = (char*) malloc(sizeof(char));
 
     char c = getchar();
+    if(c == '\n') c = getchar();
 
     while (c != '\n') {
         s[(*len)++] = c; // заносим в строку новый символ
-        // если реальный размер больше размера контейнера, то увеличим его размер
+
         if (*len >= capacity) {
             capacity *= 2;
             s = (char*) realloc(s, capacity * sizeof(char));
@@ -45,10 +45,12 @@ int main(void)
     int fd1[2];
     int fd2[2];
     int counter = 1; // Четная или нечётная строка
+    printf("Enter filename for first process\n");
     scanf("%s", filename);
-    int file1 = (filename, O_RDWR | O_CREAT, 0777);
+    int file1 = open(filename, O_RDWR | O_CREAT, 0777);
+    printf("Enter filename for second process\n");
     scanf("%s", filename);
-    int file2 = (filename, O_RDWR | O_CREAT, 0777);
+    int file2 = open(filename, O_RDWR | O_CREAT, 0777);
 
     if(file1 < 0 | file2 < 0){
         printf("One of the files opened incorrectly\n");
@@ -67,19 +69,19 @@ int main(void)
         printf("Fork failed\n");
         exit(4);
     }
-    if(!child1){
+    if(child1 == 0){
         pid_t child2 = fork();
         if (child2 == -1) {
             printf("Fork failed\n");
             exit(5);
         }
-        if(!child2){ // process2 is good
+        if(child2 == 0){ // process2 is good
             while(1){
                 int size2;
-                read(fd2[0], &size2, sizeof(int));
                 char string2[size2];
+                read(fd2[0], &size2, sizeof(int));
                 read(fd2[0], string2, sizeof(char) * size2);
-                printf("%s From child2: ", string2);
+                printf("%s -child2\n", string2);
                 Reverse(string2);
                 write(file2, string2, sizeof(char) * size2);
                 write(file2, "\n", sizeof(char));
@@ -87,26 +89,27 @@ int main(void)
         } else {   // process1 is good
             while(1) {
                 int size1;
-                read(fd1[0], &size1, sizeof(int));
                 char string1[size1];
+                read(fd1[0], &size1, sizeof(int));
                 read(fd1[0], string1, sizeof(char) * size1);
-                printf("%s From child1: ", string1);
+                printf("%s -child1\n", string1);
                 Reverse(string1);
                 write(file1, string1, sizeof(char) * size1);
                 write(file1, "\n", sizeof(char));
             }
         }
-
     } else { // Parent's process
         while(1) {
             int len;
             char *string = get_string(&len);
-            if(counter % 2){
-                write(fd2[1], &len, sizeof(int));
-                write(fd2[1], string, sizeof(char)* len)
+            if(counter % 2 == 0){
+                int len2 = strlen(string);
+                write(fd2[1], &len2, sizeof(int));
+                write(fd2[1], string, sizeof(char)* len2);
             } else{
-                write(fd1[1], &len, sizeof(int));
-                write(fd1[1], string, sizeof(char)* len)
+                int len1 = strlen(string);
+                write(fd1[1], &len1, sizeof(int));
+                write(fd1[1], string, sizeof(char)* len1);
             }
             counter++;
         }
