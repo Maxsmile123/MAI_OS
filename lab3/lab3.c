c0530e851b2184e082b130801d9b8bbbe152324f
Maxsmile123 <sysoevmaksim27@gmail.com>
2021-11-06 14:01:09 +0300

    Release version. 2.0. adding automatically generate of array, read an array of arbitrary length, parallel array splitting operation

--- lab3/lab3.c
+++ lab3/lab3.c
@@ -5,15 +5,17 @@
 #include <math.h>
 #include <string.h>
 #include <pthread.h>
+#include <time.h>
 #include "queue.h"
 
-struct send
+struct packet
 {
     int *a;
     int size;
     int l;
     int r;
 };
+
 
 void compexch(int *a, int *b)
 {
@@ -55,7 +57,7 @@ int str_to_int(char *a)
     return res;
 }
 
-int maxFromArray(int *a, const int size)
+int max_from_array(int *a, const int size)
 {
     int max = INT_MIN;
     for (int i = 0; i < size; i++) {
@@ -66,35 +68,35 @@ int maxFromArray(int *a, const int size)
 
 void *shuffle(void *arg)
 {
-    struct send *received = (struct send *) arg;
+    struct packet *received = (struct packet *) arg;
 
     int half = (int) (received->l + received->r) / 2;
     int tmp[received->size];
     int i;
-    int winRight = 0;
-    int winLeft = 0;
+    int win_right = 0;
+    int win_left = 0;
 
     for (i = 0; i < received->size; i++)
         tmp[i] = received->a[i];
 
-    while (winRight + winLeft != (received->r - received->l) + 1) {
-        if (winLeft == (received->r - received->l + 1) / 2) {
-            tmp[received->l + winRight + winLeft] = received->a[half + 1 + winRight];
-            winRight++;
+    while (win_right + win_left != (received->r - received->l) + 1) {
+        if (win_left == (received->r - received->l + 1) / 2) {
+            tmp[received->l + win_right + win_left] = received->a[half + 1 + win_right];
+            win_right++;
             continue;
         }
-        if (winRight == (received->r - received->l + 1) / 2) {
-            tmp[received->l + winRight + winLeft] = received->a[received->l + winLeft];
-            winLeft++;
+        if (win_right == (received->r - received->l + 1) / 2) {
+            tmp[received->l + win_right + win_left] = received->a[received->l + win_left];
+            win_left++;
             continue;
         }
-        if (received->a[received->l + winLeft] > received->a[half + 1 + winRight]) {
-            tmp[received->l + winRight + winLeft] = received->a[half + 1 + winRight];
-            winRight++;
+        if (received->a[received->l + win_left] > received->a[half + 1 + win_right]) {
+            tmp[received->l + win_right + win_left] = received->a[half + 1 + win_right];
+            win_right++;
         }
         else {
-            tmp[received->l + winRight + winLeft] = received->a[received->l + winLeft];
-            winLeft++;
+            tmp[received->l + win_right + win_left] = received->a[received->l + win_left];
+            win_left++;
         }
     }
     for (i = 0; i < received->size; i++)
@@ -119,26 +121,116 @@ void unshuffle(int a[], const int size, int l, int r)
         a[i] = tmp[i];
 }
 
-void OddEvenMergeSort(int a[], const int size, int l, int r)
+void *OddEvenSplitThreadEdition(void *arg)
 {
-    if (r == l + 1) compexch(&a[l], &a[r]); // мы дошли до подмассива размера 2 - теперь просто сравним элементы
-    if (r < l + 2) return; // дошли до подмассива размера 1 - выходим, такой подмассив априори отсортирован
-    unshuffle(a, size, l, r); // делим массив на две части
-    int half = (int) (l + r) / 2;
-    OddEvenMergeSort(a, size, l, half);
-    OddEvenMergeSort(a, size, half + 1, r); //вызываемся рекурсивно для половинок
+    struct packet *received = (struct packet *) arg;
+    if (received->r == received->l + 1) compexch(&received->a[received->l], &received->a[received->r]);
+    if (received->r < received->l + 2) {
+        free(received);
+        return NULL;
+    }
+    unshuffle(received->a, received->size, received->l, received->r);
+    free(received);
+    return NULL;
+}
+
+void OddEvenSplit(int a[], const int size, const int max_threads, queue *q)
+{
+    int l = 0;
+    int r = size - 1;
+    if (r == l + 1) compexch(&a[l], &a[r]);
+    if (r < l + 2) return;
+    unshuffle(a, size, l, r);
+    if(max_threads < 0) {
+        for (int i = 0; i < log2(size); i++) {
+            int j = pow(2, i);
+            l = 0;
+            r = size - 1;
+            for (int z = 0; z < i; z++) // вычисление начального значения для r
+                r = (int) (l + r) / 2;
+            for (int k = 0; k < j; k++) {
+                struct packet *s = malloc(sizeof(struct packet));
+                s->a = a;
+                s->l = l;
+                s->r = r;
+                s->size = size;
+                pthread_t *thr = malloc(sizeof(pthread_t));
+                point3:
+                {
+                    int check = pthread_create(thr, NULL, OddEvenSplitThreadEdition, (void *) s);
+                    if (check != 0) {
+                        pthread_join(*(q->first->thread), NULL);
+                        pop(q);
+                        goto point3;
+                    }
+                    else {
+                        push(q, thr);
+                    }
+                }
+                l += pow(2, log2(size) - i);
+                r += pow(2, log2(size) - i);
+            }
+            while (!isEmpty(q)) {
+                pthread_join(*(q->first->thread), NULL);
+                pop(q);
+            }
+
+        }
+    } else{
+        for (int i = 0; i < log2(size); i++) {
+            int j = pow(2, i);
+            l = 0;
+            r = size - 1;
+            for (int z = 0; z < i; z++) // вычисление начального значения для r
+                r = (int) (l + r) / 2;
+            for (int k = 0; k < j; k++) {
+                struct packet *s = malloc(sizeof(struct packet));
+                s->a = a;
+                s->l = l;
+                s->r = r;
+                s->size = size;
+                pthread_t *thr = malloc(sizeof(pthread_t));
+                point4:
+                {
+                    if (sizes(q) < max_threads) {
+                        int check = pthread_create(thr, NULL, OddEvenSplitThreadEdition, (void *) s);
+                        if (check != 0) {
+                            pthread_join(*(q->first->thread), NULL);
+                            pop(q);
+                            goto point4;
+                        }
+                        else {
+                            push(q, thr);
+                        }
+                    } else{
+                        pthread_join(*(q->first->thread), NULL);
+                        pop(q);
+                        goto point4;
+                    }
+                }
+                l += pow(2, log2(size) - i);
+                r += pow(2, log2(size) - i);
+            }
+            while (!isEmpty(q)) {
+                pthread_join(*(q->first->thread), NULL);
+                pop(q);
+            }
+
+        }
+
+    }
 }
 
 void sort(int a[], const int size, const int max_threads, queue *q)
 {
-    OddEvenMergeSort(a, size, 0, size - 1);
+    OddEvenSplit(a, size, max_threads, q);
     int l, r;
     int i, j;
     if (max_threads < 0) { // no limit
         for (i = 1; i <= log2(size); i++) {
             j = pow(2, i + 1);
             for (l = 0, r = j - 1; r < size; l += j, r += j) {
-                struct send *s = malloc(sizeof(struct send));
+                struct packet *s = malloc(sizeof(struct packet));
                 s->a = a;
                 s->l = l;
                 s->r = r;
@@ -167,7 +259,7 @@ void sort(int a[], const int size, const int max_threads, queue *q)
         for (i = 1; i < log2(size); i++) {
             j = pow(2, i + 1);
             for (l = 0, r = j - 1; r < size; l += j, r += j) {
-                struct send *s = malloc(sizeof(struct send));
+                struct packet *s = malloc(sizeof(struct packet));
                 s->a = a;
                 s->l = l;
                 s->r = r;
@@ -198,14 +290,55 @@ void sort(int a[], const int size, const int max_threads, queue *q)
                 pop(q);
             }
         }
-
     }
 
 }
 
+int fpeek(void) // Для проверки, что символ != EOF и '\n'
+{
+    int c;
+    c = fgetc(stdin);
+    if (c != EOF && c != '\n') ungetc(c, stdin);
+    return c;
+}
+
+int *get_array(int *size)
+{
+    *size = 0;
+    int number;
+    int capacity = 1;
+    int c;
+    int *a = (int *) malloc(sizeof(int));
+    printf("Enter array : \n");
+    while ((c = fpeek()) != '\n' && c != EOF) {
+        scanf("%d", &number);
+        a[(*size)++] = number;
+        if (capacity <= *size) {
+            capacity *= 2;
+            a = (int *) realloc(a, capacity * sizeof(int));
+        }
+    }
+    return a;
+}
+
+int *generate_array(int size)
+{
+    unsigned int range;
+    printf("Enter a range of numbers in the array. Max value is %d \n", INT_MAX);
+    scanf("%d", &range);
+    int *a = (int *) malloc(size * sizeof(int));
+    printf("Your array: ");
+    for (int i = 0; i < size; i++) {
+        a[i] = rand() % (2 * range + 1) - range;
+        printf("%d ", a[i]);
+    }
+    printf("\n");
+    return a;
+}
+
 int main(int argc, char *argv[])
 {
-
+    srand(time(NULL));
     int max_threads;
 
     if (argc == 1) {
@@ -232,33 +365,53 @@ int main(int argc, char *argv[])
         printf("if MAX_THREADS is negative value, then the number of threads is unlimited\n");
         return 1;
     }
+
+    char choise;
+    int flag = 1;
     int size;
     int max;
+    int *a;
 
-    printf("Enter the size of array : ");
-    scanf("%d", &size);
+    printf("Select an action:\n"
+           "1. Read the array\n"
+           "2. Generate an array automatically\n");
+    choise = getchar();
+    while (flag) {
+        switch (choise) {
+            case '1':a = get_array(&size);
+                flag = 0;
+                break;
+            case '2':printf("Enter the size of array: ");
+                scanf("%d", &size);
+                a = generate_array(size);
+                flag = 0;
+                break;
+            default:printf("Select 1 or 2 action: ");
+                choise = getchar();
+                break;
+        }
+    }
+
 
     int sizeOfArray = pow(2, (int) log2(size) + 1);
 
     if (size == sizeOfArray / 2) sizeOfArray /= 2;
-    int a[sizeOfArray];
+    else a = (int *) realloc(a, sizeOfArray * sizeof(int));
 
-    printf("Enter %d numbers of array : ", size);
-    for (int i = 0; i < size; i++)
-        scanf("%d", &a[i]);
-
-    max = maxFromArray(a, size);
+    max = max_from_array(a, size);
     for (int i = size; i < sizeOfArray; i++)
         a[i] = max;
 
     queue *q;
     create(&q);
 
-
     sort(a, sizeOfArray, max_threads, q);
 
+    printf("Sorted array: \n");
     for (int i = 0; i < size; i++)
         printf("%d ", a[i]);
     printf("\n");
+
+    free(a);
     return 0;
 }
